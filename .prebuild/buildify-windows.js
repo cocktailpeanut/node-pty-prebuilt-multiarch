80bc9ee7a97aa1458eaa81345b57453c617510e9
cocktailpeanut <cocktailpeanuts@proton.me>
2023-03-06 16:32:13 -0500

    d

--- .prebuild/buildify-windows.js
+++ .prebuild/buildify-windows.js
@@ -75,47 +75,47 @@ try {
   process.exit(0);
 }
 
-console.log("Built files for Windows");
-console.log("Extracting them into the right sub-directories in prebuilds/");
-
-// Find files (like .e.g. prebuilds/@cdktf/node-pty-prebuilt-multiarch-v0.10.1-pre.9-node-v83-darwin-x64.tar.gz)
-const pkg = require("../package.json");
-//const [scope, package] = pkg.name.split("/");
-const version = pkg.version;
-//const dir = path.join(cwd, "prebuilds", scope);
-const dir = path.join(cwd, "prebuilds", "win32-x64");
-
-
-const files = fs.readdirSync(dir);
-console.log("**FILES", files)
-const all = files.map((file) => {
-  const match = /node-v(\d+)-/.exec(file);
-  if (!match) return Promise.resolve();
-  const fullname = path.join(dir, file);
-  const abi = match[1];
-  const dest = path.resolve(
-    cwd,
-    "prebuilds",
-    `${os.platform}-${os.arch}`,
-    `abi${abi}`
-  );
-  return decompress(fullname, dest, {
-    plugins: [decompressTargz()],
-  }).then(() => {
-    console.log(`Decompressed ${fullname} into ${dest}`);
-  });
-});
-
-Promise.all(all).then(() => {
-  console.log("Done decompressing.");
-  console.log("Deleting compressed files in prebuilds dir");
-
-//  for(let file of files) {
-//    console.log("removing", file)
-//    fs.rmSync(path.join(dir, file))
-//  }
-
-  fs.rmdirSync(dir, { recursive: true, force: true });
-
-  console.log("Done");
-});
+//console.log("Built files for Windows");
+//console.log("Extracting them into the right sub-directories in prebuilds/");
+//
+//// Find files (like .e.g. prebuilds/@cdktf/node-pty-prebuilt-multiarch-v0.10.1-pre.9-node-v83-darwin-x64.tar.gz)
+//const pkg = require("../package.json");
+////const [scope, package] = pkg.name.split("/");
+//const version = pkg.version;
+////const dir = path.join(cwd, "prebuilds", scope);
+//const dir = path.join(cwd, "prebuilds", "win32-x64");
+//
+//
+//const files = fs.readdirSync(dir);
+//console.log("**FILES", files)
+//const all = files.map((file) => {
+//  const match = /node-v(\d+)-/.exec(file);
+//  if (!match) return Promise.resolve();
+//  const fullname = path.join(dir, file);
+//  const abi = match[1];
+//  const dest = path.resolve(
+//    cwd,
+//    "prebuilds",
+//    `${os.platform}-${os.arch}`,
+//    `abi${abi}`
+//  );
+//  return decompress(fullname, dest, {
+//    plugins: [decompressTargz()],
+//  }).then(() => {
+//    console.log(`Decompressed ${fullname} into ${dest}`);
+//  });
+//});
+//
+//Promise.all(all).then(() => {
+//  console.log("Done decompressing.");
+//  console.log("Deleting compressed files in prebuilds dir");
+//
+////  for(let file of files) {
+////    console.log("removing", file)
+////    fs.rmSync(path.join(dir, file))
+////  }
+//
+//  fs.rmdirSync(dir, { recursive: true, force: true });
+//
+//  console.log("Done");
+//});
